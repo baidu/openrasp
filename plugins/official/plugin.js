b6c8b86a26b23b530dbbf7c15abfe99f3811b302
Aaron Lewis <the.warl0ck.1989@gmail.com>
2017-12-01 10:11:00 +0800

    SQL检测算法增加控制开关; 改进常量对比算法

--- plugins/official/plugin.js
+++ plugins/official/plugin.js
@@ -239,101 +239,113 @@ plugin.register('sql', function (params, context) {
     var parameters = context.parameter
     var tokens     = RASP.sql_tokenize(params.query, params.server)
 
+    // console.log(tokens)
+
     // 算法1: 匹配用户输入
     // 1. 简单识别逻辑是否发生改变
     // 2. 识别数据库管理器   
-    Object.keys(parameters).some(function (name) {
-        var value = parameters[name][0]
+    if (1) {
+        Object.keys(parameters).some(function (name) {
+            var value = parameters[name][0]
 
-        if (value.length <= 10) {
-            return
-        }
+            if (value.length <= 10) {
+                return
+            }
 
-        // 判断是否为数据库管理器
-        if (value.length == params.query.length && value == params.query) {
-            reason = '算法2: WebShell - 数据库管理器'
-            return true
-        }
+            // 判断是否为数据库管理器
+            if (value.length == params.query.length && value == params.query) {
+                reason = '算法2: WebShell - 数据库管理器'
+                return true
+            }
 
-        // 简单识别用户输入
-        if (params.query.indexOf(value) == -1) {
-            return
-        }
+            // 简单识别用户输入
+            if (params.query.indexOf(value) == -1) {
+                return
+            }
 
-        // 去掉用户输入再次匹配
-        var tokens2 = RASP.sql_tokenize(params.query.replace(value, ''), params.server)
-        if (tokens.length - tokens2.length > 2) {
-            reason = '算法1: 数据库查询逻辑发生改变'
-            return true
-        }
-    })
-    if (reason !== false) {
-        return {
-            'action':     'block',
-            'confidence': 90,
-            'message':    reason
-        }
-    }
-
-    // 算法2: SQL语句策略检查（模拟SQL防火墙功能）
-    var func_list = {
-        'load_file': true,
-        'benchmark': true,
-        'sleep':     true,
-        'pg_sleep':  true
-    }
-    var tokens_lc = tokens.map(v => v.toLowerCase())
-
-    for (var i = 0; i < tokens_lc.length; i ++) {
-        if (tokens_lc[i] == ';') {
-            reason = '禁止多语句查询'
-            break
-        } else if (tokens_lc[i].indexOf('0x') == 0) {
-            reason = '禁止16进制字符串'
-            break
-        } else if (tokens_lc[i].indexOf('/*!') == 0) {
-            reason = '禁止MySQL版本号注释'
-            break
-        } else if (i < tokens_lc.length - 3 && (tokens_lc[i] == 'and' || tokens_lc[i] == 'or' || tokens_lc[i] == 'xor')) {
-            // @FIXME: 可绕过，暂时不更新
-            // 简单识别 (and|xor|or) NUMBER (>|<|>=|<=|xor) NUMBER
-            //               i       i+1         i+2        i+3
-                    
-            var op1    = tokens_lc[i + 1]
-            var op2    = tokens_lc[i + 3]
-            var method = tokens_lc[i + 2]
-
-            if (! isNaN(op1) && ! isNaN(op2) && (method[0] == '<' || method[0] == '>' || method[0] == '=')) {
-                reason = '禁止常量比较操作'
-                break
-            }                    
-        } else if (i > 0 && tokens_lc[i].indexOf('(') == 0) {
-            // @FIXME: 可绕过，暂时不更新
-            if (func_list[tokens_lc[i - 1]]) {
-                reason = '禁止执行敏感函数: ' + tokens_lc[i - 1]
-                break
+            // 去掉用户输入再次匹配
+            var tokens2 = RASP.sql_tokenize(params.query.replace(value, ''), params.server)
+            if (tokens.length - tokens2.length > 2) {
+                reason = '算法1: 数据库查询逻辑发生改变'
+                return true
+            }
+        })
+        if (reason !== false) {
+            return {
+                'action':     'block',
+                'confidence': 90,
+                'message':    reason
             }
         }
     }
 
-    if (reason !== false) {
-        return {
-            action:     'block',
-            message:    '数据库语句异常: ' + reason + '（算法3）',
-            confidence: 100
+    // 算法2: SQL语句策略检查（模拟SQL防火墙功能）
+    if (1) {
+        var func_list = {
+            'load_file': true,
+            'benchmark': true,
+            'sleep':     true,
+            'pg_sleep':  true
+        }
+        var tokens_lc = tokens.map(v => v.toLowerCase())
+
+        for (var i = 0; i < tokens_lc.length; i ++) {
+            if (tokens_lc[i] == ';') {
+                reason = '禁止多语句查询'
+                break
+            } else if (tokens_lc[i].indexOf('0x') == 0) {
+                reason = '禁止16进制字符串'
+                break
+            } else if (tokens_lc[i].indexOf('/*!') == 0) {
+                reason = '禁止MySQL版本号注释'
+                break
+            } else if (i < tokens_lc.length - 3 && 
+                (tokens_lc[i] == 'xor'
+                    || tokens_lc[i][0] == '<' 
+                    || tokens_lc[i][0] == '>' 
+                    || tokens_lc[i][0] == '=')) {
+                // @FIXME: 可绕过，暂时不更新
+                // 简单识别 NUMBER (>|<|>=|<=|xor) NUMBER
+                //          i-1         i          i+2    
+                        
+                var op1    = tokens_lc[i - 1]
+                var op2    = tokens_lc[i + 1]
+
+                if (! isNaN(op1) && ! isNaN(op2)) {
+                    reason = '禁止常量比较操作'
+                    break
+                }                    
+            } else if (i > 0 && tokens_lc[i].indexOf('(') == 0) {
+                // @FIXME: 可绕过，暂时不更新
+                if (func_list[tokens_lc[i - 1]]) {
+                    reason = '禁止执行敏感函数: ' + tokens_lc[i - 1]
+                    break
+                }
+            }
+        }
+
+        if (reason !== false) {
+            return {
+                action:     'block',
+                message:    '数据库语句异常: ' + reason + '（算法3）',
+                confidence: 100
+            }
         }
     }
 
     // 算法3: 简单正则匹配（即将移除）
-    var sqlRegex = /\bupdatexml\s*\(|\bextractvalue\s*\(|\bunion.*select.*(from|into|benchmark).*\b/i
+    if (1) {
+        var sqlRegex = /\bupdatexml\s*\(|\bextractvalue\s*\(|\bunion.*select.*(from|into|benchmark).*\b/i
 
-    if (sqlRegex.test(params.query)) {
-        return {
-            action:     'block',
-            message:    'SQL 注入攻击（算法4）',
-            confidence: 100
+        if (sqlRegex.test(params.query)) {
+            return {
+                action:     'block',
+                message:    'SQL 注入攻击（算法4）',
+                confidence: 100
+            }
         }
     }
+
     return clean
 })
 
