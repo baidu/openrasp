22c1b601a6348cb62102991deedeee5dad75927e
Aaron Lewis <the.warl0ck.1989@gmail.com>
2017-12-11 10:34:32 +0800

    检测异常请求: 爬虫、扫描器等等

---
+++ plugins/addons/003-unusual-request.js
@@ -0,0 +1,33 @@
+var plugin = new RASP('offical')
+var clean  = {
+  action: 'ignore',
+  message: '无风险',
+  confidence: 0
+}
+
+plugin.register('request', function(params, context) {
+  var header = context.header
+  var method = context.method
+  var reason = false
+
+  if (! header['accept']) {
+    reason = '缺少 Accept 请求头'
+  } else if (! header['content-type']) {
+    reason = '缺少 Content-Type 请求头'
+  } else if (method != 'get' && method != 'post' && method != 'head') {
+    reason = method.toUpperCase()
+  }
+
+  if (reason) {
+    return {
+      action:     'block',
+      message:    '不常见的请求方式: ' + reason,
+      confidence: 90
+    }
+  }
+
+  return clean
+})
+
+plugin.log('003-unusual-request 加载完成')
+
