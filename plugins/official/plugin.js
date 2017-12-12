8e1fb17c9b695bd8a3adba7dc6b850566648a9dd
Aaron Lewis <the.warl0ck.1989@gmail.com>
2017-12-12 17:40:18 +0800

    add sqlite file extension

--- plugins/official/plugin.js
+++ plugins/official/plugin.js
@@ -12,7 +12,7 @@ const clean = {
 }
 
 var forcefulBrowsing = {
-    dotFiles: /\.(7z|tar|gz|bz2|xz|rar|zip|sql|db)$/,
+    dotFiles: /\.(7z|tar|gz|bz2|xz|rar|zip|sql|db|sqlite)$/,
     nonUserDirectory: /^\/(proc|sys|root)/,
     unwantedFilenames: [
         // user files
