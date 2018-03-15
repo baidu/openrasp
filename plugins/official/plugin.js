a1f3214ccb361b459d55fe399a8728d7b2dfe508
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-03-15 19:11:06 +0800

    允许更多常量操作

--- plugins/official/plugin.js
+++ plugins/official/plugin.js
@@ -259,11 +259,11 @@ if (RASP.get_jsengine() !== 'v8') {
                     var num2 = parseInt(op2)
 
                     if (! isNaN(num1) && ! isNaN(num2)) {
-                        // 允许 1=1 这样的常量对比以避免误报
+                        // 允许 1=1, 2=0 这样的常量对比以避免误报
                         // 来自 wooyun-2015-0131345 的截图
                         // 
                         // SQLmap 是随机4位数字，不受影响
-                        if (num1 == 1 && num2 == 1 && tokens_lc[i][0] === '=')
+                        if (num1 < 10 && num2 < 10 && tokens_lc[i][0] === '=')
                         {
                             continue;
                         }
