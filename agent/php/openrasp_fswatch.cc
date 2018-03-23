0bf94513e72c91eeaa4ff0225a431a15b6a5a712
lanyuhang <lanyuhang@baidu.com>
2018-03-23 18:47:41 +0800

    fix(file monitor) only execute monitor->start() in new thread

--- agent/php/openrasp_fswatch.cc
+++ agent/php/openrasp_fswatch.cc
@@ -20,52 +20,62 @@ PHP_MINIT_FUNCTION(openrasp_fswatch)
         //openrasp_error(E_WARNING, CONFIG_ERROR, _("SAPI %s does not support automatic reloading, file monitor is deactivated"), sapi_module.name);
         return SUCCESS;
     }
-    fswatch_thread = new std::thread([&TSRMLS_C]() {
-        std::vector<std::string> paths;
-        paths.push_back(std::string(openrasp_ini.root_dir) + DEFAULT_SLASH + std::string("plugins"));
-        paths.push_back(std::string(openrasp_ini.root_dir) + DEFAULT_SLASH + std::string("assets"));
-        fsw::monitor *monitor = nullptr;
+    std::vector<std::string> paths;
+    paths.push_back(std::string(openrasp_ini.root_dir) + DEFAULT_SLASH + std::string("plugins"));
+    paths.push_back(std::string(openrasp_ini.root_dir) + DEFAULT_SLASH + std::string("assets"));
+    fsw::monitor *monitor = nullptr;
+    try
+    {
+        monitor = fsw::monitor_factory::create_monitor(
+            fsw_monitor_type::system_default_monitor_type, paths,
+            [](const std::vector<fsw::event> &events, void *ctx) {
+                if (raise(supported_sapi->second))
+                {
+                    openrasp_error(E_WARNING, CONFIG_ERROR, _("Failed to reload %s"), sapi_module.name);
+                }
+            } TSRMLS_CC);
 
+        std::vector<fsw_event_type_filter> event_filters;
+        event_filters.push_back(fsw_event_type_filter{fsw_event_flag::IsFile});
+        event_filters.push_back(fsw_event_type_filter{fsw_event_flag::Created});
+        event_filters.push_back(fsw_event_type_filter{fsw_event_flag::MovedFrom});
+        event_filters.push_back(fsw_event_type_filter{fsw_event_flag::MovedTo});
+        event_filters.push_back(fsw_event_type_filter{fsw_event_flag::Removed});
+        event_filters.push_back(fsw_event_type_filter{fsw_event_flag::Renamed});
+        event_filters.push_back(fsw_event_type_filter{fsw_event_flag::Updated});
+        monitor->set_event_type_filters(event_filters);
+
+        std::vector<fsw::monitor_filter> path_filters;
+        path_filters.push_back(fsw::monitor_filter{".*", fsw_filter_type::filter_exclude, false, false});
+        path_filters.push_back(fsw::monitor_filter{R"([/\\]plugins([/\\][^/\\]+\.js)?$)", fsw_filter_type::filter_include, false, false});
+        path_filters.push_back(fsw::monitor_filter{R"([/\\]assets([/\\]inject\.html)?$)", fsw_filter_type::filter_include, false, false});
+        monitor->set_filters(path_filters);
+
+        monitor->set_recursive(false);
+        monitor->set_latency(1);
+    }
+    catch (std::exception &conf)
+    {
+        openrasp_error(E_WARNING, FSWATCH_ERROR, _("Failed to initialize fswatch: %s"), conf.what());
+    }
+    catch (...)
+    {
+        openrasp_error(E_WARNING, FSWATCH_ERROR, _("Failed to initialize fswatch: unknown error."));
+    }
+    fswatch_thread = new std::thread([&monitor]() {
         try
         {
-            monitor = fsw::monitor_factory::create_monitor(
-                fsw_monitor_type::system_default_monitor_type, paths,
-                [](const std::vector<fsw::event> &events, void *ctx) {
-                    auto evt = std::find_if(events.begin(), events.end(), [](fsw::event evt) {
-                        auto flags = evt.get_flags();
-                        auto flag = std::find_if(flags.begin(), flags.end(), [](fsw_event_flag flag) {
-                            return flag == fsw_event_flag::IsDir || flag == fsw_event_flag::IsSymLink;
-                        });
-                        return flag == flags.end();
-                    });
-                    if (evt != events.end())
-                    {
-                        if (raise(supported_sapi->second))
-                        {
-                            openrasp_error(E_WARNING, CONFIG_ERROR, _("Failed to reload %s"), sapi_module.name);
-                        }
-                    }
-                } TSRMLS_CC);
-
-            std::vector<fsw::monitor_filter> path_filters;
-            path_filters.push_back(fsw::monitor_filter{".*", fsw_filter_type::filter_exclude, false, false});
-            path_filters.push_back(fsw::monitor_filter{R"([/\\]plugins([/\\][^/\\]+\.js)?$)", fsw_filter_type::filter_include, false, false});
-            path_filters.push_back(fsw::monitor_filter{R"([/\\]assets([/\\]inject\.html)?$)", fsw_filter_type::filter_include, false, false});
-            monitor->set_filters(path_filters);
-
-            monitor->set_recursive(false);
-            monitor->set_latency(2);
-
             monitor->start();
         }
         catch (std::exception &conf)
         {
-            openrasp_error(E_WARNING, FSWATCH_ERROR, _("Failed to initialize fswatch: %s"), conf.what());
+            openrasp_error(E_WARNING, FSWATCH_ERROR, _("Fswatch error: %s"), conf.what());
         }
         catch (...)
         {
-            openrasp_error(E_WARNING, FSWATCH_ERROR, _("Failed to initialize fswatch: unknown error."));
+            openrasp_error(E_WARNING, FSWATCH_ERROR, _("Fswatch error: unknown error"));
         }
+        monitor->stop();
         delete monitor;
         monitor = nullptr;
     });
