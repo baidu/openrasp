b6f228bdaa238f9388fe557867216fd32620e9f4
tangyouyi <tangyouyi@baidu.com>
2018-04-21 17:59:23 +0800

    modify the log of the sql connection detection

--- agent/java/src/main/java/com/fuxi/javaagent/plugin/info/SecurityPolicyInfo.java
+++ agent/java/src/main/java/com/fuxi/javaagent/plugin/info/SecurityPolicyInfo.java
@@ -53,11 +53,17 @@ public class SecurityPolicyInfo extends EventInfo {
 
     private Type policy;
     private String message;
+    private Map<String,String> params;
 
-    public SecurityPolicyInfo(Type policy, String message, boolean isBlock) {
+    public SecurityPolicyInfo(Type policy, String message, boolean isBlock, Map<String, String> params) {
         this.policy = policy;
         this.message = message;
+        this.params = params;
         setBlock(isBlock && Config.getConfig().getEnforcePolicy());
+    }
+
+    public SecurityPolicyInfo(Type policy, String message, boolean isBlock) {
+        this(policy,message,isBlock,null);
     }
 
     @Override
@@ -83,6 +89,10 @@ public class SecurityPolicyInfo extends EventInfo {
         info.put("server_version", getCatalinaServerVersion());
         // 安全规范检测信息
         info.put("message", message);
+        // 检测参数信息
+        if(params !=null){
+            info.put("params",params);
+        }
         // 攻击调用栈
         StackTraceElement[] trace = filter(new Throwable().getStackTrace());
         info.put("stack_trace", stringify(trace));
