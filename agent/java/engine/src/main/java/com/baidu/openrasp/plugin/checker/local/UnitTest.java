052baad55fdea40b9ab2c103092b4afd6c5cda37
yinhuochong <350905402@qq.com>
2018-08-21 15:56:08 +0800

    change test case to multi files

--- agent/java/engine/src/main/java/com/baidu/openrasp/plugin/checker/local/UnitTest.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/plugin/checker/local/UnitTest.java
@@ -1,6 +1,7 @@
 package com.baidu.openrasp.plugin.checker.local;
 
 import com.baidu.openrasp.plugin.info.EventInfo;
+import com.baidu.openrasp.request.AbstractRequest;
 import com.baidu.openrasp.request.UnitTestRequest;
 import com.baidu.openrasp.plugin.checker.CheckParameter;
 import com.google.gson.Gson;
@@ -25,52 +26,65 @@ public class UnitTest {
         JsonObject config = getConfig();
         JsonObject requestInfo;
         JsonObject params;
-        String input = readJsonFile(UnitTest.class.getResource("/pluginUnitTest/unitCases.json").getPath());
-        Gson gson = new Gson();
-        Iterator testCases = gson.fromJson(input, JsonElement.class).getAsJsonArray().iterator();
-        while(testCases.hasNext()) {
-            requestInfo = (JsonObject)testCases.next();
-            params = (JsonObject)requestInfo.get("params");
+        int success = 0, fail = 0, ignore = 0;
+        LinkedList<String> testCaseFiles = getJsonFiles(UnitTest.class.getResource("/pluginUnitTest/unitCases").getPath());
+        for(String fileName : testCaseFiles){
 
-            Map<String, Object> paramMap = new HashMap<String, Object>();
-            for (Map.Entry<String, JsonElement> entrySet : params.entrySet()) {
-                if(entrySet.getValue().isJsonArray()){
-                    Iterator<JsonElement> stringArray = entrySet.getValue().getAsJsonArray().iterator();
-                    LinkedList<String> stringArrayList = new LinkedList<String>();
-                    while(stringArray.hasNext()){
-                        stringArrayList.add(stringArray.next().getAsString());
+            String input = readJsonFile(fileName);
+            Gson gson = new Gson();
+            Iterator testCases = gson.fromJson(input, JsonElement.class).getAsJsonArray().iterator();
+            while(testCases.hasNext()) {
+                requestInfo = (JsonObject) testCases.next();
+                params = (JsonObject) requestInfo.get("params");
+                //获取case中的param
+                Map<String, Object> paramMap = new HashMap<String, Object>();
+                for (Map.Entry<String, JsonElement> entrySet : params.entrySet()) {
+                    if (entrySet.getValue().isJsonArray()) {
+                        Iterator<JsonElement> stringArray = entrySet.getValue().getAsJsonArray().iterator();
+                        LinkedList<String> stringArrayList = new LinkedList<String>();
+                        while (stringArray.hasNext()) {
+                            stringArrayList.add(stringArray.next().getAsString());
+                        }
+                        paramMap.put(entrySet.getKey(), stringArrayList);
+                    } else {
+                        paramMap.put(entrySet.getKey(), entrySet.getValue().getAsString());
                     }
-                    paramMap.put(entrySet.getKey(), stringArrayList);
                 }
-                else{
-                    paramMap.put(entrySet.getKey(), entrySet.getValue().getAsString());
+                //构造AbstractRequest
+                UnitTestRequest testRequest = new UnitTestRequest(requestInfo);
+                //构造CheckParameter
+                CheckParameter checkParameter = new CheckParameter(null, paramMap);
+
+                //根据测试类型，调用对应的检测方法
+                String testUnitName = requestInfo.get("name").getAsString();
+                List<EventInfo> result;
+                if (testUnitName.equals("sql")) {
+                    SqlStatementChecker sqlStatementChecker = new SqlStatementChecker();
+                    result = sqlStatementChecker.testCheckSql(checkParameter, testRequest.getParameterMap(), config);
+                } else if (testUnitName.equals("ssrf")) {
+                    SSRFChecker ssrfChecker = new SSRFChecker();
+                    result = ssrfChecker.testCheckSSRF(checkParameter, testRequest.getParameterMap(), config);
+                } else {
+                    //忽略没有对应方法的用例
+                    //System.out.println("[IGNORED] Test id:" + requestInfo.get("id").getAsString());
+                    ignore ++;
+                    continue;
                 }
-            }
-            UnitTestRequest testRequest = new UnitTestRequest(requestInfo);
-            CheckParameter checkParameter = new CheckParameter(null, paramMap);
-            String testUnitName = requestInfo.get("name").getAsString();
-            List<EventInfo> result;
-            if(testUnitName.equals("sql")){
-                SqlStatementChecker sqlStatementChecker = new SqlStatementChecker();
-                result = sqlStatementChecker.testCheckSql(checkParameter, testRequest.getParameterMap(), config);
-            }
-            else if(testUnitName.equals("ssrf")){
-                SSRFChecker ssrfChecker = new SSRFChecker();
-                result = ssrfChecker.testCheckSSRF(checkParameter, testRequest.getParameterMap(), config);
-            }
-            else{
-                System.out.println("[IGNORED] Test id:" + requestInfo.get("id").getAsString());
-                continue;
-            }
-            String action = requestInfo.get("action").getAsString();
-            if(((action.equals("block") || action.equals("log")) && result.size() != 0)
-                    || (action.equals("ignore")) && result.size() == 0){
-                System.out.println("[PASS] Test id:" + requestInfo.get("id").getAsString() );
-            }
-            else {
-                System.out.println("[FAILED] Test id:" + requestInfo.get("id").getAsString());
+                String action = requestInfo.get("action").getAsString();
+
+                if (((action.equals("block") || action.equals("log")) && result.size() != 0)
+                        || (action.equals("ignore")) && result.size() == 0) {
+                    success ++;
+                    //System.out.println("[PASS] Test id:" + requestInfo.get("id").getAsString());
+                } else {
+                    fail ++;
+                    System.out.println("[FAILED] Test id:" + requestInfo.get("id").getAsString());
+                    System.out.println("Case description:" + requestInfo.get("description").getAsString());
+                }
             }
         }
+        String output = String.format("Test finished. %d cases succeed, %d cases failed, %d cases ignored.", success, fail, ignore);
+        System.out.println(output);
     }
 
     private static String readJsonFile(String path) throws IOException{
@@ -81,6 +95,24 @@ public class UnitTest {
         reader.read(chars);
         return String.valueOf(chars);
     }
+
+    public static LinkedList<String> getJsonFiles(String pathName) throws IOException{
+        File dirFile = new File(pathName);
+        if (! dirFile.exists() || ! dirFile.isDirectory()) {
+            throw new IOException();
+        }
+        String[] fileList = dirFile.list();
+        LinkedList<String> result = new LinkedList<String>();
+        for (int i = 0; i < fileList.length; i++) {
+            String string = fileList[i];
+            File file = new File(dirFile.getPath(), string);
+            if (!file.isDirectory()) {
+                result.push(dirFile.getPath() + File.separator + string);
+            }
+        }
+        return result;
+    }
+
 
     public static JsonObject getConfig() throws Exception{
         String configJson = readJsonFile(UnitTest.class.getResource("/pluginUnitTest/unitConfig.json").getPath());
