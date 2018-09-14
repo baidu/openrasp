ceae83b2d649dcff328666c5563edb68c7bbb742
anyang <anyang@baidu.com>
2018-09-14 20:40:14 +0800

    modify dubbo params

--- agent/java/engine/src/main/java/com/baidu/openrasp/hook/dubbo/DubboRequestHook.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/hook/dubbo/DubboRequestHook.java
@@ -67,7 +67,7 @@ public class DubboRequestHook extends AbstractClassHook {
                 if (parameterTypes[i].isPrimitive() || isWrapClass(parameterTypes[i]) || args[i] instanceof String) {
                     String[] strings = new String[1];
                     strings[0] = String.valueOf(args[i]);
-                    map.put(String.valueOf(i), strings);
+                    map.put("openrasp-dubbo-"+i, strings);
                 }
 
             }
