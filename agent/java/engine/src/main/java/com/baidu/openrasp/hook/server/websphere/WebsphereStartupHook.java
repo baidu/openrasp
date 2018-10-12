9a020b1ec8c4c2b91dd7a83cd5130f5deafa8114
anyang <anyang@baidu.com>
2018-10-12 10:45:17 +0800

    增加websphere基线检查

--- agent/java/engine/src/main/java/com/baidu/openrasp/hook/server/websphere/WebsphereStartupHook.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/hook/server/websphere/WebsphereStartupHook.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2017-2018 Baidu Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.baidu.openrasp.hook.server.websphere;
 
 import com.baidu.openrasp.HookHandler;
 import com.baidu.openrasp.hook.server.ServerStartupHook;
+import com.baidu.openrasp.plugin.checker.CheckParameter;
 import com.baidu.openrasp.tool.Reflection;
 import com.baidu.openrasp.tool.annotation.HookAnnotation;
 import com.baidu.openrasp.tool.model.ApplicationModel;
@@ -37,6 +54,6 @@ public class WebsphereStartupHook extends ServerStartupHook {
         } catch (Exception e) {
             HookHandler.LOGGER.warn("handle websphere startup failed", e);
         }
-
+        HookHandler.doCheckWithoutRequest(CheckParameter.Type.POLICY_WEBSPHERE_START, CheckParameter.EMPTY_MAP);
     }
 }
