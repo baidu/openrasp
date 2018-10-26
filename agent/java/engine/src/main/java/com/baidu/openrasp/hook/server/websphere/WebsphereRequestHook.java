f93b86fcf34898463fd3a4a2a21bf0daabcb4f35
anyang <anyang@baidu.com>
2018-10-26 10:21:56 +0800

    modify websphere deploy war

--- agent/java/engine/src/main/java/com/baidu/openrasp/hook/server/websphere/WebsphereRequestHook.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/hook/server/websphere/WebsphereRequestHook.java
@@ -34,14 +34,13 @@ public class WebsphereRequestHook extends ServerRequestHook {
 
     @Override
     public boolean isClassMatched(String className) {
-        return "com/ibm/ws/webcontainer/servlet/ServletWrapper".equals(className);
+        return "com/ibm/ws/webcontainer/filter/WebAppFilterManager".equals(className);
     }
 
     @Override
     protected void hookMethod(CtClass ctClass) throws IOException, CannotCompileException, NotFoundException {
-        String hookDesc = "(Ljavax/servlet/ServletRequest;Ljavax/servlet/ServletResponse;)V";
         String src = getInvokeStaticSrc(ServerRequestHook.class, "checkRequest",
                 "$0,$1,$2", Object.class, Object.class, Object.class);
-        insertBefore(ctClass, "handleRequest", hookDesc, src);
+        insertBefore(ctClass, "invokeFilters", null, src);
     }
 }
