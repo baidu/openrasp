f63842e403952994221d01d77ccda8f23b5fefd4
anyang <anyang@baidu.com>
2018-11-28 19:25:36 +0800

    modify register

--- agent/java/engine/src/main/java/com/baidu/openrasp/hook/server/jboss/JBossStartupHook.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/hook/server/jboss/JBossStartupHook.java
@@ -60,10 +60,10 @@ public class JBossStartupHook extends ServerStartupHook {
         try {
             String serverVersion = Reflection.invokeStringMethod(object, "getVersionNumber", new Class[]{});
             ApplicationModel.init("jboss", serverVersion);
-            sendRegister();
         } catch (Exception e) {
             HookHandler.LOGGER.error("handle jboss startup failed", e);
         }
+        sendRegister();
         HookHandler.doPolicyCheckWithoutRequest(CheckParameter.Type.POLICY_JBOSS_START, CheckParameter.EMPTY_MAP);
     }
 }
