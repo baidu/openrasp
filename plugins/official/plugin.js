3d8eae80b80885df3aea421749c82f2f46f60c17
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-12-11 16:17:02 +0800

    增加说明

--- plugins/official/plugin.js
+++ plugins/official/plugin.js
@@ -176,8 +176,9 @@ var algorithmConfig = {
     },
     // 任意文件下载防护 - 使用 ../../ 跳出 web 目录读取敏感文件
     readFile_outsideWebroot: {
-        name:   '算法4 - 禁止使用 ../../ 访问web目录以外的文件',
-        action: 'ignore'
+        name:      '算法4 - 禁止使用 ../../ 访问web目录以外的文件',
+        action:    'ignore',
+        reference: 'https://rasp.baidu.com/doc/dev/official.html#case-out-webroot'
     },
     // 任意文件下载防护 - 读取敏感文件，最后一道防线
     readFile_unwanted: {
