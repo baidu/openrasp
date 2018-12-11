1f8cdc12b2a76032594963ad1dfbf92fbd9225a5
lanyuhang <lanyuhang@baidu.com>
2018-12-11 16:36:10 +0800

    fix(build): use npm ci instead of npm install

--- build-cloud.sh
+++ build-cloud.sh
@@ -55,7 +55,7 @@ function build_cloud()
 function build_vue()
 {
     cd rasp-vue
-    npm install --unsafe-perm
+    npm ci --unsafe-perm
     npm run build
 
     rm -rf ../cloud/src/rasp-cloud/dist
