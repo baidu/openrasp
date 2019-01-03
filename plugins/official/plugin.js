7406e6433efc993504db8e959e52c86a93d742af
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-01-03 19:10:06 +0800

    copyright修正

--- plugins/official/plugin.js
+++ plugins/official/plugin.js
@@ -2,7 +2,7 @@ const plugin_version = '2018-1227-1200'
 const plugin_name    = 'official'
 
 /*
- * Copyright 2017-2018 Baidu Inc.
+ * Copyright 2017-2019 Baidu Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
