8c0c0eb56e7b2f6499604519b53f375cbe7fb2bb
anyang666 <35440503+anyang666@users.noreply.github.com>
2019-01-17 21:06:44 +0800

    add spring-boot CI (#162)

--- agent/java/integration-test/build-war.sh
+++ agent/java/integration-test/build-war.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-jar cvf app.war -C app/ .
+if [[ $SERVER == "springboot" ]]; then
+    cp app/*.jsp springboot-jsp-master/src/main/webapp
+
+    pushd springboot-jsp-master/
+
+    mvn clean package
+
+    popd
+else
+	jar cvf app.war -C app/ .
+fi
---
+++ agent/java/integration-test/springboot-jsp-master/src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -0,0 +1,19 @@
+package com.hellokoding.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
+
