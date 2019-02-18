ed23698fb0fd8672c702190790a173689722ef62
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-02-18 13:58:12 +0800

    修正编译脚本

--- build-java.sh
+++ build-java.sh
@@ -48,19 +48,19 @@ function buildPlugin {
 }
 
 function copyConf {
-	cp $BASE_DIR/rasp-install/java/src/main/resources/rasp.yaml $OUTPUT_ROOT/rasp/conf/rasp.yaml || exit 1
+	cp $BASE_DIR/rasp-install/java/src/main/resources/openrasp.yml $OUTPUT_ROOT/rasp/conf/openrasp.yml || exit 1
 }
 
 log "[1] build RaspInstall.jar" 
 buildRaspInstall
 
-log "[2] copy OpenRasp Plugin"
+log "[2] copy OpenRASP Plugin"
 buildPlugin
 
 log "[3] copy rasp.yaml"
 copyConf
 
-log "[4] build OpenRasp"
+log "[4] build OpenRASP"
 buildRasp
 
 cd $OUTPUT_ROOT/..
