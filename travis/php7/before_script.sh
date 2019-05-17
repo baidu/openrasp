74bb80b4af9243ea3781163496a872c4052cf070
lanyuhang <lanyuhang@baidu.com>
2019-05-17 17:10:06 +0800

    test(travis): update php build script

--- travis/php7/before_script.sh
+++ travis/php7/before_script.sh
@@ -10,9 +10,8 @@ psql -c "ALTER USER postgres WITH PASSWORD 'postgres';" -U postgres ;
 psql -c "CREATE USER openrasp WITH PASSWORD '123456'" -U postgres;
 
 #openrasp-v8
-git clone --depth=1 https://github.com/baidu-security/openrasp-v8.git /tmp/openrasp-v8
-mkdir -p /tmp/openrasp-v8/php/build
-pushd /tmp/openrasp-v8/php/build
+mkdir -p $TRAVIS_BUILD_DIR/openrasp-v8/php/build
+pushd $TRAVIS_BUILD_DIR/openrasp-v8/php/build
 cmake ..
 make -j2 --quiet
 popd
@@ -20,5 +19,5 @@ popd
 pushd agent/$OPENRASP_LANG
 phpenv config-rm xdebug.ini || true
 phpenv config-rm ext-opcache.ini || true
-phpize && ./configure --with-openrasp-v8=/tmp/openrasp-v8 --with-gettext --enable-openrasp-remote-manager --enable-cli-support && make -j2
+phpize && ./configure --with-openrasp-v8=$TRAVIS_BUILD_DIR/openrasp-v8 --with-gettext --enable-openrasp-remote-manager --enable-cli-support && make -j2
 popd
