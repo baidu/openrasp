7f3d2bb98549a9be28e6ed3358371bf4325f5d6b
lixinkai <lixinkai@baidu.com>
2019-05-24 14:35:56 +0800

    [PHP5]mongo travis

--- travis/php5/before_script.sh
+++ travis/php5/before_script.sh
@@ -9,6 +9,31 @@ ln -s /run/mysqld/mysqld.sock /tmp/mysql.sock
 psql -c "ALTER USER postgres WITH PASSWORD 'postgres';" -U postgres ;
 psql -c "CREATE USER openrasp WITH PASSWORD '123456'" -U postgres;
 
+#mongo
+sleep 15
+mongo test --eval 'db.createUser({user:"openrasp",pwd:"rasp#2019",roles:["readWrite"]});'
+
+#mongo-php-driver-legacy
+git clone https://github.com/mongodb/mongo-php-driver-legacy.git
+pushd mongo-php-driver-legacy
+phpize
+./configure
+make all
+sudo make install
+echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+popd
+
+#mongo-php-driver
+git clone https://github.com/mongodb/mongo-php-driver.git
+pushd mongo-php-driver
+git submodule update --init
+phpize
+./configure
+make all
+sudo make install
+echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+popd
+
 pushd agent/$OPENRASP_LANG
 phpenv config-rm xdebug.ini || true
 phpenv config-rm ext-opcache.ini || true
