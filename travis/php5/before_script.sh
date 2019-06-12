18d6ff7e7e40757d0654b51c08c574e096dbef59
lanyuhang <lanyuhang@baidu.com>
2019-06-12 15:26:10 +0800

    feat(php): update openrasp-v8

--- travis/php5/before_script.sh
+++ travis/php5/before_script.sh
@@ -9,6 +9,11 @@ ln -s /run/mysqld/mysqld.sock /tmp/mysql.sock
 psql -c "ALTER USER postgres WITH PASSWORD 'postgres';" -U postgres ;
 psql -c "CREATE USER openrasp WITH PASSWORD '123456'" -U postgres;
 
+#cmake
+wget -N https://github.com/Kitware/CMake/releases/download/v3.14.5/cmake-3.14.5-Linux-x86_64.tar.gz -P $HOME/cache
+tar zxf $HOME/cache/cmake-3.14.5-Linux-x86_64.tar.gz -C /tmp
+export PATH=/tmp/cmake-3.14.5-Linux-x86_64/bin:$PATH
+
 #openrasp-v8
 mkdir -p $TRAVIS_BUILD_DIR/openrasp-v8/build
 pushd $TRAVIS_BUILD_DIR/openrasp-v8/build
