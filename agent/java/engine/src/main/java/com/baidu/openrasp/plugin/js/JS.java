2379d743a3635fcb54ef7f4adc8e57f4db156498
lanyuhang <lanyuhang@baidu.com>
2019-06-26 12:22:21 +0800

    feat(java): support RASP.get_stack method

--- agent/java/engine/src/main/java/com/baidu/openrasp/plugin/js/JS.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/plugin/js/JS.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2019 Baidu Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.baidu.openrasp.plugin.js;
 
 import com.google.gson.JsonElement;
@@ -27,7 +43,9 @@ import com.baidu.openrasp.plugin.checker.CheckParameter;
 import com.baidu.openrasp.plugin.checker.CheckParameter.Type;
 import com.baidu.openrasp.tool.filemonitor.FileScanListener;
 import com.baidu.openrasp.tool.filemonitor.FileScanMonitor;
+import com.baidu.openrasp.tool.StackTrace;
 import com.baidu.openrasp.v8.V8;
+import com.baidu.openrasp.v8.StackGetter;
 import com.baidu.openrasp.v8.ByteArrayOutputStream;
 
 public class JS {
@@ -45,12 +63,26 @@ public class JS {
             if (!V8.Initialize()) {
                 throw new Exception("V8 Worker threads do not started");
             }
-            V8.SetPluginLogger(new com.baidu.openrasp.v8.Logger() {
+            V8.SetLogger(new com.baidu.openrasp.v8.Logger() {
                 @Override
                 public void log(String msg) {
                     PLUGIN_LOGGER.info(msg);
                 }
             });
+            V8.SetStackGetter(new com.baidu.openrasp.v8.StackGetter() {
+                @Override
+                public byte[] get() {
+                    try {
+                        ByteArrayOutputStream stack = new ByteArrayOutputStream();
+                        JsonStream.serialize(StackTrace.getStackTraceArray(), stack);
+                        stack.write(0);
+                        return stack.getByteArray();
+                    } catch (Exception e) {
+                        return null;
+                    }
+                }
+            });
+            Context.setKeys();
             if (!CloudUtils.checkCloudControlEnter()) {
                 UpdatePlugin();
                 InitFileWatcher();
