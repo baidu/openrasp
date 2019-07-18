53637e974221c145124a709c5db10c24e1abdd6f
yinhuochong <350905402@qq.com>
2019-07-18 12:20:43 +0800

    [插件] eval_regex 单测时默认log

--- plugins/official/plugin.js
+++ plugins/official/plugin.js
@@ -451,7 +451,7 @@ var algorithmConfig = {
 
     eval_regex: {
         name:   '算法1 - 正则表达式',
-        action: 'log',
+        action: 'ignore',
         regex:  'base64_decode|gzuncompress|create_function'
     },
 }
@@ -572,6 +572,9 @@ if (! RASP.is_unittest)
         // 关闭 xss_echo 非攻击过滤
         algorithmConfig.xss_echo.filter_regex = ""
     }
+}
+else {
+    algorithmConfig.eval_regex.action = "log"
 }
 
 // 校验 sql_regex 正则是否合法
