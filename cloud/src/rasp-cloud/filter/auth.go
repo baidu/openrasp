fc4cb12cc11a200b6f8107a170a479eab9b0e0aa
tangouyi <tangyouyi@baidu.com>
2019-10-10 18:01:29 +0800

    modify the rasp-cloud

--- cloud/src/rasp-cloud/filter/auth.go
+++ cloud/src/rasp-cloud/filter/auth.go
@@ -26,7 +26,7 @@ func init() {
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type", "X-OpenRASP-Token"},
 		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
 		AllowCredentials: true,
 	}))
