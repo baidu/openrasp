e4a6458fc56bcb02c2840a6ded2edf0832236b51
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-10-29 15:37:42 +0800

    版本号统一改为 1.2.2

--- agent/php7/openrasp.cc
+++ agent/php7/openrasp.cc
@@ -49,7 +49,7 @@ using openrasp::ConfigHolder;
 
 ZEND_DECLARE_MODULE_GLOBALS(openrasp);
 
-const char *OpenRASPInfo::PHP_OPENRASP_VERSION = "1.2.1";
+const char *OpenRASPInfo::PHP_OPENRASP_VERSION = "1.2.2";
 bool is_initialized = false;
 bool remote_active = false;
 static bool make_openrasp_root_dir();
