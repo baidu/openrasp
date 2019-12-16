c89e0887c62e2dae6355d63538173d921d060322
tangouyi <tangyouyi@baidu.com>
2019-12-16 15:26:38 +0800

    modify the cloud timer task for rasp-java

--- agent/java/engine/src/main/java/com/baidu/openrasp/cloud/CloudTimerTask.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/cloud/CloudTimerTask.java
@@ -28,15 +28,20 @@ public abstract class CloudTimerTask implements Runnable {
     public void run() {
         while (isAlive) {
             try {
-                execute();
+                try {
+                    execute();
+                } catch (Throwable t) {
+                    handleError(t);
+                }
+                try {
+                    // 和上面分开处理，避免心跳失败不走 sleep,不能放到 execute 之前，会导致第一次心跳不能马上运行
+                    Thread.sleep(sleepTime * 1000);
+                } catch (Throwable t) {
+                    handleError(t);
+                }
             } catch (Throwable t) {
-                handleError(t);
-            }
-            try {
-                // 和上面分开处理，避免心跳失败不走 sleep,不能放到 execute 之前，会导致第一次心跳不能马上运行
-                Thread.sleep(sleepTime * 1000);
-            } catch (InterruptedException e) {
-                handleError(e);
+                System.out.println("OpenRASP cloud task failed: " + t.getMessage());
+                t.printStackTrace();
             }
         }
     }
