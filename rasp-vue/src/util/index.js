a98a9245056e75f6752ea1cce56ff6b4f2a6c664
Aaron Lewis <the.warl0ck.1989@gmail.com>
2019-12-19 19:28:04 +0800

    [前端] 文案

--- rasp-vue/src/util/index.js
+++ rasp-vue/src/util/index.js
@@ -135,7 +135,7 @@ export var attack_types = {
   webshell_command: 'WebShell - 命令执行',
   webshell_file_put_contents: 'WebShell - 后门上传',
   webshell_ld_preload: 'WebShell - LD_PRELOAD 后门',
-  response: 'HTTP 响应检测'
+  response: 'HTTP 响应采样检测'
 }
 
 export var status_types = {
