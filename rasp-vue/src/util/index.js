3c71499db6f56a9945da27848b4cf3358cd02170
Aaron Lewis <the.warl0ck.1989@gmail.com>
2020-01-15 14:38:27 +0800

    [前端] 增加任意文件删除

--- rasp-vue/src/util/index.js
+++ rasp-vue/src/util/index.js
@@ -121,6 +121,7 @@ export var attack_types = {
   directory: '目录遍历',
   rename: '文件重命名',
   readFile: '任意文件下载',
+  deleteFile: '任意文件删除',
   include: '任意文件包含',
   writeFile: '任意文件写入',
   ssrf: 'SSRF 服务端请求伪造',
