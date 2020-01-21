7d657511f2c1487fcc5989f54003b44603b2de3f
Erwin Darg <gmhlovehadoop@163.com>
2020-01-21 16:58:08 +0800

    支持最大天数配置

--- cloud/src/rasp-cloud/filter/access.go
+++ cloud/src/rasp-cloud/filter/access.go
@@ -62,6 +62,7 @@ func formatTime(timestamp int64, format string) (times string) {
 func initAccessLogger() {
 	logPath := "logs/access"
 	maxSize := strconv.FormatInt(conf.AppConfig.LogMaxSize, 10)
+	maxDays := strconv.Itoa(conf.AppConfig.LogMaxDays)
 	if isExists, _ := tools.PathExists(logPath); !isExists {
 		err := os.MkdirAll(logPath, os.ModePerm)
 		if err != nil {
@@ -73,7 +74,7 @@ func initAccessLogger() {
 	accessLogger.EnableFuncCallDepth(true)
 	accessLogger.SetLogFuncCallDepth(4)
 	err := accessLogger.SetLogger(logs.AdapterFile,
-		`{"filename":"`+logPath+`/access.log","daily":true,"maxdays":10,"perm":"0777","maxsize": `+maxSize+`}`)
+		`{"filename":"`+logPath+`/access.log","daily":true,"maxdays":`+maxDays+`,"perm":"0777","maxsize": `+maxSize+`}`)
 	if err != nil {
 		tools.Panic(tools.ErrCodeLogInitFailed, "failed to init access log", err)
 	}
