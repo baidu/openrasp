ef7267bf952d9494319bedf81c23671a840e93b2
lixin1234qqq <lixin1234qqq@gmail.com>
2021-02-09 07:10:57 +0000

    [PHP]test

--- travis/php7/script.sh
+++ travis/php7/script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ev
 
+ulimit -c unlimited -S
+mkdir -p /tmp/coredump
+sudo bash -c "echo '/tmp/coredump/core.%p.%E' > /proc/sys/kernel/core_pattern"
+
 pushd agent/$OPENRASP_LANG
 php run-tests.php -p `which php` -d extension=`pwd`/modules/openrasp.so --offline --show-diff --set-timeout 120
 popd
