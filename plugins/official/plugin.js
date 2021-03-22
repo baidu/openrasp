9cb9ccebdd205ebd647eaf342ae6f89975b2bc72
commiter <commit@github.com>
2021-03-22 14:34:14 +0800

    [插件] 增加dnslog地址 https://github.com/baidu/openrasp/issues/258

--- plugins/official/plugin.js
+++ plugins/official/plugin.js
@@ -1,4 +1,4 @@
-const plugin_version = '2021-0310-1500'
+const plugin_version = '2021-0322-1500'
 const plugin_name    = 'official'
 const plugin_desc    = '官方插件'
 
@@ -241,8 +241,11 @@ var algorithmConfig = {
             '.godns.vip',
             '.dnslog.cn',
             '.0kee.360.cn',
-            '.xn--9tr.com',  // yumusb/DNSLog-Platform-Golang
-            '.pipedream.net' // requestbin 新地址
+            '.xn--9tr.com',   // yumusb/DNSLog-Platform-Golang
+            '.pipedream.net', // requestbin 新地址
+            '.vxtrans.com',
+            '.vxtrans.link',
+            '.r87.me'
         ]
     },
     // SSRF - 是否允许访问混淆后的IP地址
