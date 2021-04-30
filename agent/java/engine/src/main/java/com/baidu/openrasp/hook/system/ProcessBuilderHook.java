b581362ade1398048d7bca2c4a64ceffff45a875
Xuing <admin@xuing.cn>
2021-04-30 17:08:59 +0800

    [java] 修复windows下命令执行的hook问题

--- agent/java/engine/src/main/java/com/baidu/openrasp/hook/system/ProcessBuilderHook.java
+++ agent/java/engine/src/main/java/com/baidu/openrasp/hook/system/ProcessBuilderHook.java
@@ -76,13 +76,13 @@ public class ProcessBuilderHook extends AbstractClassHook {
     @Override
     protected void hookMethod(CtClass ctClass) throws IOException, CannotCompileException, NotFoundException {
         if (ctClass.getName().contains("ProcessImpl")) {
-            if (ModuleLoader.isModularityJdk()) {
+            if (OSUtil.isWindows() || !ModuleLoader.isModularityJdk()) {
                 String src = getInvokeStaticSrc(ProcessBuilderHook.class, "checkCommand",
-                        "$1,$2,$4", byte[].class, byte[].class, byte[].class);
+                        "$1,$2", String[].class, String.class);
                 insertBefore(ctClass, "<init>", null, src);
             } else {
                 String src = getInvokeStaticSrc(ProcessBuilderHook.class, "checkCommand",
-                        "$1,$2", String[].class, String.class);
+                        "$1,$2,$4", byte[].class, byte[].class, byte[].class);
                 insertBefore(ctClass, "<init>", null, src);
             }
         } else if (ctClass.getName().contains("UNIXProcess")) {
