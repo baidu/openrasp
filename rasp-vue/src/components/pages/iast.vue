594f7944a8cabae7091adea4773986ff8f54c2c7
commiter <commit@github.com>
2021-12-23 11:05:34 +0800

    [前端] 更新IAST描述

--- rasp-vue/src/components/pages/iast.vue
+++ rasp-vue/src/components/pages/iast.vue
@@ -111,9 +111,9 @@
             </tbody>
           </table>
 
-          <p v-if="! loading && register == 0" class="text-center" v-model="register">扫描器未连接或已离线</p>
+          <p v-if="! loading && register == 0" class="text-center" v-model="register">扫描器未连接或已离线，<a href="https://rasp.baidu.com/doc/install/iast.html" target="_blank">请参考文档进行配置</a></p>
           <p v-if="! loading && total == 0 && register == 1" class="text-center" v-model="register">连接中</p>
-          <p v-if="! loading && total == 0 && register == 2" class="text-center" v-model="register">扫描器已连接，暂无数据</p>
+          <p v-if="! loading && total == 0 && register == 2" class="text-center" v-model="register">扫描器已连接，暂无数据。可尝试触发一些请求，<a href="https://rasp.baidu.com/doc/install/iast.html#faq-no-task" target="_blank">或者参考文档排查</a></p>
           <p v-if="! loading && register == 3" class="text-center" v-model="register">响应接收异常</p>
           <p v-if="! loading && register == 4" class="text-center" v-model="register">扫描器连接超时</p>
 
